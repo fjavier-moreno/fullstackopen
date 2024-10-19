6cc71370dc2403804e18c388fa6f72ca8debe20e
Francisco Javier Moreno <fjavier.moreno@outlook.com>
2024-10-19 22:46:37 +0200

    Exercise 1.4

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 const Header = (course) => {
   return (
-    <h1>{course.name}</h1>
+    <h1>{course.course}</h1>
   )
 }
 
@@ -39,24 +39,26 @@ const Total = ({ parts }) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
-      <Header name={course} />
-      <Content parts={[part1, part2, part3]} />
-      <Total parts={[part1, part2, part3]} />
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
