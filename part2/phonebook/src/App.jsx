3de84a873484611bee4044d52ac0b761a0192a9d
Francisco Javier Moreno <fjavier.moreno@outlook.com>
2024-10-23 20:08:03 +0200

    Exercise 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -4,11 +4,24 @@ import Contacts from './components/Contacts'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
+  const [filter, setFilter] = useState([...persons])
   const [newnumber, setNewNumber] = useState('')
 
+  const handleContactSearch = (event) => {
+    const searchTerm = event.target.value.toLowerCase()
+  
+    if (searchTerm.length > 0) {
+      setFilter(persons.filter((person) => person.name.toLowerCase().includes(searchTerm)))
+    } else {
+      setFilter(persons)
+    }
+  }
 
   const handleContactChange = (event) => {
     setNewName(event.target.value)
@@ -24,14 +37,17 @@ const App = () => {
     if (persons.find((person) => person.name === newName)) {
       alert(`${newName} is alrady added to phonebook`)
     } else {
-      const contactObject = { name: newName, number: newnumber}
+      const contactObject = { name: newName, number: newnumber, id: newName}
       setPersons(persons.concat(contactObject))
+      setFilter(persons.concat(contactObject))
     }
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <p>filter shown with <input onChange={handleContactSearch} /></p>
+      <h2>Add a new</h2>
       <form onSubmit={addContact}>
         <div>
           name: <input onChange={handleContactChange} />
@@ -44,7 +60,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <Contacts persons={persons} />
+      <Contacts persons={filter} />
     </div>
   )
 }
