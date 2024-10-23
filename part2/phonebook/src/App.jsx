8be77dd32946fc8e64050ee1ecfa1435af05b67f
Francisco Javier Moreno <fjavier.moreno@outlook.com>
2024-10-24 01:55:31 +0200

    Exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,19 +1,23 @@
-import { useState } from 'react'
-
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import NewContactForm from './components/NewContactForm'
 import Contacts from './components/Contacts'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [filter, setFilter] = useState([...persons])
   const [newnumber, setNewNumber] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+        setFilter(response.data)
+      })
+  }, [])
 
   const handleContactSearch = (event) => {
     const searchTerm = event.target.value.toLowerCase()
